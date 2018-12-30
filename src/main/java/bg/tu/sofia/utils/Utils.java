73ee00dd09e03c42c5a3909a19555992a5964af0
Denislav Veizov <denislavveizov@gmail.com>
2018-12-30 20:06:49 +0200

    Upgraded spring boot and added covers to automatically uploaded books

--- src/main/java/bg/tu/sofia/utils/Utils.java
+++ src/main/java/bg/tu/sofia/utils/Utils.java
@@ -3,16 +3,29 @@ package bg.tu.sofia.utils;
 import bg.tu.sofia.model.Blobs;
 import bg.tu.sofia.model.Book;
 import bg.tu.sofia.model.BookCategory;
+import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageTree;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
+import java.awt.image.*;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Timestamp;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
 
 public class Utils {
     private static Logger LOG = LoggerFactory.getLogger(Utils.class);
@@ -40,29 +53,32 @@ public class Utils {
         book.setNumberOfPages(getNumberOfPages(blobs.getContent()));
         book.setCreatedDate(currentDate);
         book.setLastUpdate(currentDate);
+        Blobs coverOfPdf = getCoverOfPdf(file);
+        if (Objects.nonNull(coverOfPdf))
+            book.setCover(coverOfPdf);
         return book;
     }
 
-    private static PDDocument covertToPdDocument(byte[] bytes){
+    private static PDDocument covertToPdDocument(byte[] bytes) {
         PDDocument doc;
         try {
             doc = PDDocument.load(bytes);
         } catch (IOException e) {
-            LOG.error(e.getMessage(),e);
-            throw new RuntimeException(e.getMessage(),e);
+            LOG.error(e.getMessage(), e);
+            throw new RuntimeException(e.getMessage(), e);
         }
         return doc;
     }
 
-    public static Integer getNumberOfPages(byte[] bytes){
+    public static Integer getNumberOfPages(byte[] bytes) {
         PDDocument pdDocument = covertToPdDocument(bytes);
-        if(null == pdDocument)
+        if (null == pdDocument)
             return null;
         int numberOfPages = pdDocument.getNumberOfPages();
         try {
             pdDocument.close();
         } catch (IOException e) {
-            LOG.error(e.getMessage(),e);
+            LOG.error(e.getMessage(), e);
         }
         return numberOfPages;
     }
@@ -74,8 +90,36 @@ public class Utils {
         return new Date(cal.getTime().getTime());
     }
 
-    public static String getRequestURL(HttpServletRequest request){
-       return request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
+    public static String getRequestURL(HttpServletRequest request) {
+        return request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
     }
 
+    private static Blobs getCoverOfPdf(File file) {
+        Path path = Paths.get(file.getAbsolutePath());
+        try {
+            byte[] bytes = Files.readAllBytes(path);
+            PDDocument pdDocument = Utils.covertToPdDocument(bytes);
+            PDFRenderer pdfRenderer = new PDFRenderer(pdDocument);
+            BufferedImage bim = pdfRenderer.renderImageWithDPI(0, 300, ImageType.RGB);
+            if(Objects.isNull(bim))
+                return null;
+            
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            ImageIO.write(bim, "png", out);
+
+            Blobs blobs = new Blobs();
+            blobs.setFilename(file.getName() + "-Cover");
+            blobs.setContentType("image/png");
+            blobs.setContent(out.toByteArray());
+            blobs.setCreatedDate(new Date());
+            blobs.setFilesize(out.size());
+
+            out.close();
+            return blobs;
+        } catch (IOException e) {
+            LOG.error(e.getMessage(), e);
+        }
+
+        return null;
+    }
 }
